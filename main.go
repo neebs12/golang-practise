10f91462d9a1f9266a75566c8ef8bc9f7c512299
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-08 09:14:47 +1200

    playing with Printf

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	const foo = 'h'
+	const bar = 'e'
+	fmt.Printf("Is type %T \n", foo)
+	fmt.Println(foo)
+	fmt.Println(bar)
+	fmt.Println(foo + bar)
+
+	var baz = 4.20
+	fmt.Printf("is %T\n", baz)
+
+	fmt.Printf("I am feeling 70%% today\n")
 }
