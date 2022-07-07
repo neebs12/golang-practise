b2ba972e02f9fe4b3543cdb885d85c040a0792a2
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-08 07:50:57 +1200

    update

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	t "time"
-)
-
 func main() {
-	rand.Seed(t.Now().Unix()) // <--- for time elapsed in seconds from 1970
-	name := "Jason"
-	age := 26
-	temp := rand.Float32() * 20
 
-	fmt.Printf("I am %v, I am aged %d, and I feel %.2f Celcius \n", name, age, temp)
-
-	statement := fmt.Sprintf("I am %v, I am aged %d, and I feel %.2f Celcius \n", name, age, temp)
-
-	fmt.Println(statement)
-
-	fmt.Println("Hello world")
-	fmt.Println(t.Now())
 }
