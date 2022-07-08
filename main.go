e1eddc4a9d49837ed511fb2ad90df3b033d3c82d
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-08 12:20:04 +1200

    playing with inline numeric type coercion

--- main.go
+++ main.go
@@ -2,21 +2,23 @@ package main
 
 import (
 	"fmt"
-	"bufio"
-	"os"
-	"strconv"
 )
 
 func main() {
   // try to get name, and add a greeting to it
-  scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("Please type your name here: ")
-	scanner.Scan()
-	input := scanner.Text()
-	fmt.Printf("Good morning %v!!\n", input)
-	
-	fmt.Print("What year were you born? ")
-	scanner.Scan()
-	secondInput, _ := strconv.ParseInt(scanner.Text(), 10, 32)
-	fmt.Printf("You are %d years old!\n", 2022 - secondInput)
+	var num1, num2 int = 99, 5
+	num3 := num1 / num2
+	fmt.Printf("the number is %d\n", num3)
+
+	num4 := float32(num1) / float32(num2)
+	fmt.Printf("the number is %.2f\n", num4)
+
+	// num5 := num1 / float32(num2) 
+	// <--- error due to mismatched types
+
+	var num6 float32 = 3.2
+	var num7 int = 3
+	// fmt.Println("This is boolean: %t", num6 == num1)
+	// <--- error due to mismatched types
+	fmt.Printf("This is boolean: %t\n", int(num6) == num7)
 }
