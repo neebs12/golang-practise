ce02b4cbf445d12f214be1fbe249d4f89a42dcdd
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-09 05:35:14 +1200

    arrays when using assg opr are COPIED, not pass by ref-d

--- main.go
+++ main.go
@@ -2,26 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
-func greetAndAdd(greet string, n1, n2 int) string {
-	return fmt.Sprintf("%s: %d", greet, n1+n2)
-}
-
-func myRange(n1, n2 int) (less int, more int) {
-	less = n1 - n2
-	more = n1 + n2
-	return
-}
-
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < 10; i += 1 {
-		firstNum := rand.Intn(100)
-		secondNum := rand.Intn(100)
-		// fmt.Println(greetAndAdd("Hello", firstNum, secondNum))
-		fmt.Println(myRange(firstNum, secondNum))
-	}
+	arr := [2]int{11, 22}
+	arr2 := arr
+
+	fmt.Println(arr, arr2) // [11, 22] [11, 22]
+	arr[0] = 1000
+	fmt.Println(arr, arr2) // [1000, 22] [11, 22]
 }
