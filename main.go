45473baf7abb88f7ab55a55e39c399c93b865ea6
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-08 14:12:31 +1200

    playing with slices and arrays

--- main.go
+++ main.go
@@ -5,28 +5,28 @@ import (
 )
 
 func main() {
-	var arr [3]int
-	// arr = {11, 22, 33} // <-- no
-	// arr = [11, 22, 33] // <-- no
-	arr[0] = 11 // yes
-	arr[1] = 22
-	arr[2] = 33 
-	fmt.Println(arr, len(arr))
+	arr := [6]int{9, 11, 6, 42, 23, 600}
+	sli := arr[2:5]
+	fmt.Printf("An array is of type: %T, a slice is of type: %T\n", arr, sli)
+	// <--- [6]int is an array, []int is a slice
+	fmt.Println(arr, sli)
+	sli2 := arr[:]
+	// arr[len(arr) - 1] = 599 // <-- affects all refs
+	fmt.Println(arr, sli2)
+	sli3 := sli[1:2]
+	fmt.Println(sli3)
+	sli4 := sli[:cap(sli)] // expands to parent array
+	fmt.Println(sli4)
 
-	sum := 0
-	for i := 0; i < len(arr); i += 1{
-		sum += arr[i]
-	}
-	fmt.Println(sum)
+	var sli5 []int = []int{11, 22, 33, 44, 55}
+	fmt.Println(sli5)
 
-	arr2D := [3][2]int{{11, 22}, {33, 44}, {55, 66}}
-	fmt.Println(arr2D, len(arr2D))
+	fmt.Println("-----------------")
+	sli6 := append(sli, 100)
+	// <--- appending value MUST be of same type as elements of the appending array
+	// <--- think, only way that a collection can be accepted is if the first arg is a 2D collection or higher
+	fmt.Println(sli6)
 
-	sum = 0 // reassg
-	for i := 0 ; i < len(arr2D); i += 1{
-		for j := 0; j < len(arr2D[i]); j += 1{
-			sum += arr2D[i][j]
-		}
-	}
-	fmt.Println(sum)
+	sli7 := make([]int, 5)
+	fmt.Println(sli7)
 }
