a690742822bae90fc10a7357fa8c39acf8f1f528
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-08 16:17:57 +1200

    playing around with function definitions

--- main.go
+++ main.go
@@ -2,42 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+func greetAndAdd(greet string, n1, n2 int) string {
+	return fmt.Sprintf("%s: %d", greet, n1+n2)
+}
+
+func myRange(n1, n2 int) (less int, more int) {
+	less = n1 - n2
+	more = n1 + n2
+	return
+}
+
 func main() {
-	var mp map[string]int = map[string]int{
-		"jason": 26,
-		"val": 25,
-		"isaac": 31, 
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < 10; i += 1 {
+		firstNum := rand.Intn(100)
+		secondNum := rand.Intn(100)
+		// fmt.Println(greetAndAdd("Hello", firstNum, secondNum))
+		fmt.Println(myRange(firstNum, secondNum))
 	}
-
-	fmt.Println(mp, len(mp))
-	// <--- notice, unordered
-	// <--- notice, use of len is valid
-
-	value, ok := mp["jason"]
-	if ok {
-		fmt.Printf("%s is %d years old\n", "jason", value)
-	}
-	// <--- checking if something exists
-
-	mp["jason"] = 27
-	fmt.Println(mp["jason"])
-
-	mp["tim"] = 22
-	fmt.Println(mp, len(mp))
-	// <--- incr in len
-
-	delete(mp, "jason")
-	delete(mp, "val")
-	delete(mp, "isaac")
-	fmt.Println(mp, len(mp))
-	// <--- successful deletion
-
-	mp2 := make(map[string]int)
-	// <--- another pattern to make maps
-	mp2["launch school"] = 10
-	mp2["dev academy"] = 8
-	mp2["auckland uni"] = 3 
-	fmt.Println(mp2)
 }
