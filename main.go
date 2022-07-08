8f16ef0f634d5de14e951912e30fbb1e867792b4
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-08 14:55:36 +1200

    playing with maps

--- main.go
+++ main.go
@@ -5,15 +5,39 @@ import (
 )
 
 func main() {
-	// write an algo that (only) prints the duplicates in an array
-	sli := []int{11, 4, 44, 564, 654, 11, 10, 6, 4, 4, 9}
-
-	for i, elm := range sli {
-		for _, elm2 := range sli[i+1:] {
-			if elm == elm2 {
-				fmt.Println(elm)
-				break
-			}
-		}
+	var mp map[string]int = map[string]int{
+		"jason": 26,
+		"val": 25,
+		"isaac": 31, 
 	}
+
+	fmt.Println(mp, len(mp))
+	// <--- notice, unordered
+	// <--- notice, use of len is valid
+
+	value, ok := mp["jason"]
+	if ok {
+		fmt.Printf("%s is %d years old\n", "jason", value)
+	}
+	// <--- checking if something exists
+
+	mp["jason"] = 27
+	fmt.Println(mp["jason"])
+
+	mp["tim"] = 22
+	fmt.Println(mp, len(mp))
+	// <--- incr in len
+
+	delete(mp, "jason")
+	delete(mp, "val")
+	delete(mp, "isaac")
+	fmt.Println(mp, len(mp))
+	// <--- successful deletion
+
+	mp2 := make(map[string]int)
+	// <--- another pattern to make maps
+	mp2["launch school"] = 10
+	mp2["dev academy"] = 8
+	mp2["auckland uni"] = 3 
+	fmt.Println(mp2)
 }
