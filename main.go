7ef19afcf17e58ac9f74b9e23e695ec0ccead8a4
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-09 10:59:13 +1200

    playing with interfaces

--- main.go
+++ main.go
@@ -1,37 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-type Student struct {
-	name          string
-	grades        []int
-	isFourthGrade bool
+type shape interface {
+	getArea() float32
 }
 
-// Assign struct methods
-func (s Student) getName() string {
-	return s.name
+type shape2 interface {
+	getArea() float32
+	getParam() float32
 }
 
-func (s *Student) setGrades(grades []int) {
-	// <--- note: is a pointer! this is to change values within the struct!
-	s.grades = grades
+type circle struct {
+	radius float32
 }
 
-func (s Student) getAverageGrade() float32 {
-	var sum int
-	for _, grade := range s.grades {
-		sum += grade
-	}
-	return float32(sum) / float32(len(s.grades))
+type rectangle struct {
+	side1 float32
+	side2 float32
+}
+
+func (c *circle) getArea() float32 {
+	return math.Pi * (c.radius * c.radius)
+}
+
+func (c *circle) getParam() float32 {
+	return 2 * math.Pi * c.radius
+}
+
+func (r *rectangle) getArea() float32 {
+	return r.side1 * r.side2
 }
 
 func main() {
-	std1 := Student{"Jason", []int{11, 22, 33, 44}, true}
-	std2 := Student{"Jimmy", []int{88, 99, 42, 60}, false}
-	fmt.Println(std1.getName(), std1.getAverageGrade(), std1)
-	fmt.Println(std2.getName(), std2.getAverageGrade(), std2)
+	myCircle := circle{3.5}
+	myRect := rectangle{4.24, 9.85}
 
-	std1.setGrades([]int{66, 77, 88})
-	fmt.Println(std1.getName(), std1.getAverageGrade(), std1)
+	myShapes := []shape{&myCircle, &myRect}
+
+	for _, val := range myShapes {
+		fmt.Println(val.getArea())
+	}
+
+	fmt.Println("------------------------")
+
+	myCircle2 := circle{8.95}
+	myShapes2 := []shape2{&myCircle, &myCircle2}
+	// <--- not valid to incl myRect due to missing struct method
+	for _, val := range myShapes2 {
+		fmt.Println(val.getParam())
+	}
 }
