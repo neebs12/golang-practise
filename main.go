4424be9cb9832b4d5b7a50b5bf97d39f0eeaf7dd
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-10 09:19:22 +1200

    playing with overloading

--- main.go
+++ main.go
@@ -1,56 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
-type shape interface {
-	getArea() float32
+type human struct{}
+
+func (h human) greet(str string) {
+	fmt.Printf("Hello, I hope we have found ourselves in a good place in this rocky planet, here take this word: %v\n", str)
 }
 
-type shape2 interface {
-	getArea() float32
-	getParam() float32
+type modernHuman struct {
+	*human
 }
 
-type circle struct {
-	radius float32
-}
-
-type rectangle struct {
-	side1 float32
-	side2 float32
-}
-
-func (c *circle) getArea() float32 {
-	return math.Pi * (c.radius * c.radius)
-}
-
-func (c *circle) getParam() float32 {
-	return 2 * math.Pi * c.radius
-}
-
-func (r *rectangle) getArea() float32 {
-	return r.side1 * r.side2
+func (mh modernHuman) greet(flt float32) {
+	fmt.Printf("Good day to you! We aren't primitives! Take this more sophisticated number! %.2f\n", flt)
 }
 
 func main() {
-	myCircle := circle{3.5}
-	myRect := rectangle{4.24, 9.85}
-
-	myShapes := []shape{&myCircle, &myRect}
-
-	for _, val := range myShapes {
-		fmt.Println(val.getArea())
-	}
-
-	fmt.Println("------------------------")
-
-	myCircle2 := circle{8.95}
-	myShapes2 := []shape2{&myCircle, &myCircle2}
-	// <--- not valid to incl myRect due to missing struct method
-	for _, val := range myShapes2 {
-		fmt.Println(val.getParam())
-	}
+	h := &human{}
+	h.greet("flabergasted")
+	mh := &modernHuman{}
+	mh.greet(2)
+	mh.greet(2.2)
+	// mh.greet("Hello")  <--- not allowed, not really overloaded
 }
