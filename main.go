75baabc89b4e467513e82100e98dddc5daa41c2c
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-10 11:47:54 +1200

    playing w 2D slices

--- main.go
+++ main.go
@@ -2,23 +2,23 @@ package main
 
 import "fmt"
 
-func whatAmI(val interface{}) {
-	switch val := val.(type) {
-	case int:
-		fmt.Println("I am an integer")
-	case bool:
-		fmt.Println("I am a boolean")
-	case string:
-		fmt.Println("I am a string")
-	default:
-		fmt.Printf("I cant tell what %T is\n", val)
-	}
-}
-
 func main() {
-	whatAmI(100)
-	whatAmI(true)
-	whatAmI(!!true)
-	whatAmI("hello")
-	whatAmI(1.1)
+	counter := 0
+	foo := make([][]int, 3)
+	// gives [[] [] []]
+
+	for i := 0; i < len(foo); i += 1 {
+		innerAry := make([]int, i+1)
+		foo[i] = innerAry
+		for j := 0; j < len(innerAry); j += 1 {
+			innerAry[j] = counter
+			counter += 1
+		}
+	}
+
+	fmt.Println(foo)
+
+	// make 2D, filled with values
+	// [[0], [1, 2], [3, 4, 5]]
+
 }
