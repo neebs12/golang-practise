dbf74274ed1cb364a80701a3c61215e94d5a3211
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-10 13:45:27 +1200

    playing w spread like operator - variadic functions

--- main.go
+++ main.go
@@ -2,23 +2,20 @@ package main
 
 import "fmt"
 
+func getIntSum(nums ...int) int {
+	// nums is a collection array/slice, does not matter?
+	sum := 0
+	for _, val := range nums {
+		sum += val
+	}
+	return sum
+}
+
 func main() {
+	foo := []int{11, 11, 22, 33, 44}
+	fmt.Println(getIntSum(foo...)) // <--- yes, 121
+	// <--- ok slices
 
-	for ind, val := range [3]int{11, 22, 33} {
-		fmt.Println(ind, val)
-	}
-
-	for ind := range make([]int, 3) {
-		fmt.Println(ind)
-	}
-
-	mp := map[string]float32{"height": 1.79, "temp": 14.3}
-	for key, val := range mp {
-		fmt.Println(key, val)
-	}
-
-	for _, unicode := range "jason" {
-		fmt.Println(unicode)
-		fmt.Printf("unicode is of type %T\n", unicode) // int32
-	}
+	bar := [5]int{11, 11, 22, 33, 44}
+	fmt.Println(getIntSum(bar...)) // <--- no
 }
