f0f28f21cd6cd78737e667ff899911136557b9e5
Jason Aricheta <jason.aricheta@gmail.com>
2022-07-25 04:13:08 +1200

    bin tree root

--- data-structures-juminlee/binarytree.go
+++ data-structures-juminlee/binarytree.go
@@ -1,5 +1,12 @@
 package main
 
+import "fmt"
+
+// key has the current value
+// -- left Node key has value less than current key
+// -- right Node key has value greater than current key
+// -- if Node either/both references other Nodes, is a parent
+// -- if Node neither refereces other Nodes, is a leaf
 type Node struct {
 	Key   int
 	Left  *Node
@@ -19,7 +26,14 @@ type BinaryTree struct {
 // func (b *BinaryTree) Search(i int) bool {}
 
 // Init, initializes the Binary Tree with a Node, taking in a value
+func Init(k int) *BinaryTree {
+	tree := &BinaryTree{
+		&Node{Key: k},
+	}
+	return tree
+}
 
 func main() {
-
+	tree := Init(100)
+	fmt.Println(tree)
 }
